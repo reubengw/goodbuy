076cea95db71b2e5e2ea95ff5b3bd5da4fea01e4
unknown <42800940+reubengw@users.noreply.github.com>
2020-11-10 00:16:51 +1300

    Stock.js added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import Stock from 'Stock.js';
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          
           Learn React
         </a>
       </header>
