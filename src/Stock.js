008d60fdbe4d9bb9737b36b56c8d6e8addf51b0f
unknown <42800940+reubengw@users.noreply.github.com>
2020-11-10 00:25:34 +1300

    added Stock.js

---
+++ src/Stock.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const stock = (props) =>{
+    return (
+        <div>
+            <p>stock: {props.name} price:  {props.price}</p>
+        </div>
+    )
+}
+
+export default stock;
